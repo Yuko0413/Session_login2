17ffdf33bd4d8ee2388dbf9a7dcedd72c3661f9b
Yuko0413 <soc13soc0422@gmail.com>
2024-06-14 16:51:05 +0900

    4回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root 'tasks#index'
+  
   resources :tasks
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
-  get '/login', to: 'sessions#new'
+
+  resources :sessions, only: [:create, :destroy]
+
+  resources :users, only: [:create, :show, :edit, :update, :destroy]
+
+  get '/login', to: 'sessions#new', as: 'new_session'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new', as: 'new_user'
 end
+
+
