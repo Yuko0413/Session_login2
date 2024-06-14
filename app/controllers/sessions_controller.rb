404500c350b60a58a3d82558c88e7b361fca70c6
Yuko0413 <soc13soc0422@gmail.com>
2024-06-14 13:39:52 +0900

    2回目

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = 'ログインしました'
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードに誤りがあります'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_path
+  end
+
+end
