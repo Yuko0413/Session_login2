fd6bae2322a865726f65e1ef1a2091b4ef224388
Yuko0413 <soc13soc0422@gmail.com>
2024-06-15 14:41:27 +0900

    プレフィックス

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   
   resources :tasks
 
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
-  resources :users, only: [:create, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 
   #get '/login', to: 'sessions#new', as: 'new_session'
   #post '/login', to: 'sessions#create'
