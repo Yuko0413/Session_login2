295b341f125618bc04bb6cfe5c1a8075362c3265
Yuko0413 <soc13soc0422@gmail.com>
2024-06-17 10:39:58 +0900

    バリデーション外した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  
+
   def new
     @user = User.new
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_secure_password
 
+  #validates :name, presence: true, uniqueness: true
+  #validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+
   validates :name, presence: { message: "名前を入力してください" }
   validates :email, presence: { message: "メールアドレスを入力してください" }, uniqueness: { message: "メールアドレスはすでに使用されています" }
   validates :password, presence: { message: "パスワードを入力してください" }, length: { minimum: 6, message: "パスワードは6文字以上で入力してください" }, allow_nil: true, confirmation: { message: "パスワード（確認）とパスワードの入力が一致しません" }
-  validates :password_confirmation, presence: { message: "パスワード（確認）を入力してください" }, if: -> { password.present? }
+  #validates :password_confirmation, presence: { message: "パスワード（確認）を入力してください" }, if: -> { password.present? }
 end
